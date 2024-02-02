1def10420b6946779309e9d4337c3c2678efa64f
BuildTools <m.amokhtaari@gmail.com>
2024-02-02 23:56:40 +0330

    several changes

--- back/Hamsafar/Hamsafar/settings.py
+++ back/Hamsafar/Hamsafar/settings.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
     ]
-}
+}2
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
